f40906e559c132290829d4bdb007b94321b35d2c
Kranti Prasad <krantiprasad07@gmail.com>
2021-07-03 15:14:18 +0530

    style fixed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 	const [con, setCon] = useState([]);
 	const date = new Date().toLocaleString();
 	const [statsFor, setStatsFor] = useState("1");
+	const [isLoaded, setIsLoaded] = useState(false);
 
 	useEffect(() => {
 		fetch("https://covid-india-cases.herokuapp.com/states/")
@@ -30,6 +31,7 @@ function App() {
 				nationStats[0].cured = json.recovered;
 				nationStats[0].deaths = json.deaths;
 				setCon(nationStats);
+				setIsLoaded(true);
 			});
 
 		fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states")
@@ -43,20 +45,27 @@ function App() {
 	function handleStatsChange() {
 		statsFor === "1" ? setStatsFor("2") : setStatsFor("1");
 	}
-	return (
-		<div className="main">
-			<div className="stats-section">
-				<h1>{statsFor === "1" ? "India's " : "All State/UTs "} Statistics</h1>
-				Click<span onClick={handleStatsChange}> here </span>to check{" "}
-				{statsFor === "2" ? "India's " : "all State/UTs "} Statistics.
+	if (!isLoaded) {
+		return <div id="loader"></div>;
+	} else {
+		return (
+			<div className="main">
+				<div className="stats-section">
+					<h1>{statsFor === "1" ? "India's " : "All State/UTs "} Statistics</h1>
+					Click<span onClick={handleStatsChange}> here </span>to check{" "}
+					{statsFor === "2" ? "India's " : "all State/UTs "} Statistics.
+				</div>
+				<Statistic stats={statsFor === "1" ? con : stats} date={date} />
+				<hr style={{ width: 300, marginTop: 20, marginBottom: 20 }} />
+				<div className="stats-section">
+					<h1>Track Vaccine</h1>
+				</div>
+				<div className="drop-down" style={{ paddingBottom: 150 }}>
+					<StateDropDown stateList={options} />
+				</div>
 			</div>
-			<Statistic stats={statsFor === "1" ? con : stats} date={date} />
-			<hr style={{ width: 300 }} />
-			<div className="drop-down">
-				<StateDropDown stateList={options} />
-			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
